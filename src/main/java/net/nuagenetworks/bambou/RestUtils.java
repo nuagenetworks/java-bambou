a870a56836f675aa408e9a33a0f62e6060627b61
Martin Leclerc <martin.f.leclerc@gmail.com>
2017-02-17 14:15:00 -0500

    Added toString() to new RestUtils class.

--- src/main/java/net/nuagenetworks/bambou/RestUtils.java
+++ src/main/java/net/nuagenetworks/bambou/RestUtils.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import net.nuagenetworks.bambou.util.BambouUtils;
+
 public class RestUtils {
-    
+
     public static <T extends RestObject> T createRestObjectWithContent(Class<T> restObjectClass, JsonNode jsonNode) throws RestException {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
@@ -13,5 +15,9 @@ public class RestUtils {
         } catch (JsonProcessingException ex) {
             throw new RestException(ex);
         }
+    }
+
+    public static String toString(Object content) throws RestException {
+        return BambouUtils.toString(content);
     }
 }
